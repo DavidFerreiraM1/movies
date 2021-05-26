71e028a33b008e329e9808f0867f8eea77e3dbf6
David Ferreira <davidferreira.m1@outlook.com>
2021-05-25 21:04:54 -0300

    types definidos globalmente

---
+++ src/interfaces/movies.ts
@@ -0,0 +1,38 @@
+export interface PopularMovies {
+  title: string;
+  year: number;
+  ids: {
+    trakt: number;
+    slug: string;
+    imdb: string;
+    tmdb: number;
+  };
+}
+
+export interface TrendingMovies {
+  watchers: number;
+  movie: {
+    title: string;
+    year: number;
+    ids: {
+      trakt: number;
+      slug: string;
+      imdb: string;
+      tmdb: number;
+    };
+  };
+}
+
+export interface RecommendedMovies {
+  user_count: number;
+  movie: {
+    title: string;
+    year: number;
+    ids: {
+      trakt: number;
+      slug: string;
+      imdb: string;
+      tmdb: number;
+    };
+  };
+}
