26891318db72e47d736b65700a4bbb85d7b07e45
David Ferreira <davidferreira.m1@outlook.com>
2021-05-25 23:17:40 -0300

    redux provider criado

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {PrincialScreen} from './src/features';
+import store from './src/redux';
 
 export default function App() {
-  return <PrincialScreen />;
+  return (
+    <Provider store={store}>
+      <PrincialScreen />
+    </Provider>
+  );
 }
