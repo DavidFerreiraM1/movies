27cfb50d2ea422122e57e3fad7fbc9ce7b529679
David Ferreira <davidferreira.m1@outlook.com>
2021-05-26 02:50:37 -0300

    título da sessão ajustado

--- src/features/princial-screen/styles.ts
+++ src/features/princial-screen/styles.ts
@@ -27,12 +27,49 @@ export const CardContent = styled.ScrollView`
   overflow: hidden;
 `;
 
-export const Section = styled.View`
+export const SectionRoot = styled.View`
   margin-top: 12px;
   margin-bottom: 12px;
-  background-color: red;
+  padding-horizontal: 8px;
   width: 100%;
   height: 280px;
+`;
+
+export const SectionTitle = styled.Text`
+  color: #000000;
+  font-family: Roboto-Regular;
+  font-size: 18px;
+`;
+
+export const SectionMovieSliderContainer = styled.View`
+  height: 280px;
+  padding-vertical: 8px;
+`;
+
+export const SectionMovieSlider = styled.FlatList`
+  height: 100%;
+`;
+
+export const SectionMovieSliderItem = styled.View`
+  width: 160px;
+  height: 240px;
+  margin-horizontal: 8px;
+  border-radius: 8px;
+`;
+
+export const SliderItemImage = styled.Image`
+  height: 100%;
+  width: 100%;
+  border-radius: 8px;
+`;
+
+export const MovieNameText = styled.Text`
+  font-size: 14px;
+  color: #000000;
+  font-family: Roboto-Regular;
+  text-align: center;
+  text-align-vertical: center;
+  padding-vertical: 4px;
 `;
 
 export const styles = StyleSheet.create({
