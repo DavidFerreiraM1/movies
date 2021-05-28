388060ab9ca78426e93fe2b979b7ba40f160b57c
David Ferreira <davidferreira.m1@outlook.com>
2021-05-28 04:55:30 -0300

    config de tendÇencias no redux

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {popularMovieReducer} from './movies/popular';
 import {recommendedMovieReducer} from './movies/recommended';
+import {trendingMovieReducer} from './movies/trending';
 
 export default combineReducers({
   popularMovieReducer,
   recommendedMovieReducer,
+  trendingMovieReducer,
 });
--- src/redux/root-saga.ts
+++ src/redux/root-saga.ts
@@ -3,10 +3,13 @@ import {PopularMovieTypes} from './movies/popular/types';
 import {pupularMovieRequestSaga} from './movies/popular/sagas';
 import {RecommendedMovieTypes} from './movies/recommended/types';
 import {recommendedMovieRequestSaga} from './movies/recommended/sagas';
+import {TrendingMovieTypes} from './movies/trending/types';
+import {trendingMovieRequestSaga} from './movies/trending/sagas';
 
 export default function* rootSagas(): any {
   return yield all([
     takeLatest(PopularMovieTypes.LOAD_REQUEST, pupularMovieRequestSaga),
     takeLatest(RecommendedMovieTypes.LOAD_REQUEST, recommendedMovieRequestSaga),
+    takeLatest(TrendingMovieTypes.LOAD_REQUEST, trendingMovieRequestSaga),
   ]);
 }
