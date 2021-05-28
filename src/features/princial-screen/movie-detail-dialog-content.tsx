2875c71d7a64992b200fdf9853b1922401c48247
David Ferreira <davidferreira.m1@outlook.com>
2021-05-27 23:35:00 -0300

    conteúdo de detalhes de filme iniciado

---
+++ src/features/princial-screen/movie-detail-dialog-content.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {MovieDetailDialogContentRoot} from './styles';
+
+export function MovieDetailDialogContent() {
+  return (
+    <MovieDetailDialogContentRoot>
+      <></>
+    </MovieDetailDialogContentRoot>
+  );
+}
