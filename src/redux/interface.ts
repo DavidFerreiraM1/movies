1ef302e9b20fafb552d025dd7fe59a5419dc14d3
David Ferreira <davidferreira.m1@outlook.com>
2021-05-28 04:55:55 -0300

    config redux para filmes em tendencias

--- src/redux/interface.ts
+++ src/redux/interface.ts
@@ -1,7 +1,9 @@
 import {PopularMovieState} from './movies/popular/types';
 import {RecommendedMovieState} from './movies/recommended/types';
+import {TrendingMovieState} from './movies/trending/types';
 
 export interface AppStore {
   popularMovieReducer: PopularMovieState;
   recommendedMovieReducer: RecommendedMovieState;
+  trendingMovieReducer: TrendingMovieState;
 }
