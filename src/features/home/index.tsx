2199280ddd20870f548e4ef5aa99db59a2b16f9a
David Ferreira <davidferreira.m1@outlook.com>
2021-05-29 13:15:08 -0300

    home redefinida

---
+++ src/features/home/index.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {useDispatch} from 'react-redux';
+import BackgroundImage from '../../assets/svg/background-1.svg';
+import {popularMovieRequestAction} from '../../redux/movies/popular/action';
+
+import {BackgroundRoot, CardContent, CardRoot, Root, styles} from './styles';
+import {PopularMovieSection} from './popular-movie-section';
+import {renderWithContext} from '../../util/render-with-context';
+import {ContextMoviesProvider} from './context-movies';
+import {MovieDetailDialog} from './movie-detail-dialog-content';
+import {FinderType, MovieDetailDialogRef} from './interfaces';
+import {RecommendedMovieSection} from './recommended-movie-section';
+import {recommendedMovieRequestAction} from '../../redux/movies/recommended/action';
+import {TrendingMovieSection} from './trending-movie-section';
+import {trendingMovieRequestAction} from '../../redux/movies/trending/action';
+
+export function PrincialScreenWithRef() {
+  const dispatchAction = useDispatch();
+  const movieDetailDialogRef = React.createRef<MovieDetailDialogRef>();
+
+  React.useEffect(() => {
+    dispatchAction(popularMovieRequestAction());
+    dispatchAction(recommendedMovieRequestAction());
+    dispatchAction(trendingMovieRequestAction());
+  }, []);
+
+  const openMovieDetail = (finder: FinderType) => (item: number) => {
+    movieDetailDialogRef.current?.openDetail(item, finder);
+  };
+
+  return (
+    <Root>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="rgba(0,0,0,0)"
+      />
+      <BackgroundRoot>
+        <BackgroundImage height="100%" />
+      </BackgroundRoot>
+      <CardRoot>
+        <CardContent contentContainerStyle={styles.cardContentContainer}>
+          <PopularMovieSection openDetail={openMovieDetail('popular')} />
+          <RecommendedMovieSection openDetail={openMovieDetail('recommend')} />
+          <TrendingMovieSection openDetail={openMovieDetail('trending')} />
+        </CardContent>
+      </CardRoot>
+      <MovieDetailDialog ref={movieDetailDialogRef} />
+    </Root>
+  );
+}
+
+export const PrincialScreen = renderWithContext(ContextMoviesProvider)(
+  PrincialScreenWithRef,
+);
---
+++ src/features/home/interfaces.ts
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export interface ComponentWithProps {
+  children?: React.ReactNode;
+}
+
+export interface RenderItemParams {
+  item: any;
+  index: number;
+}
+
+export type FinderType = 'popular' | 'recommend' | 'trending';
+
+export interface MovieDetailDialogRef {
+  openDetail(item: number, finder: FinderType): void;
+}
+
+export interface MovieSectionProps {
+  openDetail(item: number): void;
+}
+
+export interface ContextInterface {
+  movieId: number | null;
+  setMovieId(id: number | null): void;
+}
+
+export interface MovieDetailStateData {
+  id: string;
+  urlImg: string;
+  title: string;
+  description: string;
+}
