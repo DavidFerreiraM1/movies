2b3f6b7ddbd6a8e6b7a1fbe1281ff4e0798b72dd
David Ferreira <davidferreira.m1@outlook.com>
2021-05-29 12:29:26 -0300

    import das variáveis de acesso

--- src/http-client/index.ts
+++ src/http-client/index.ts
@@ -1,8 +1,7 @@
 import Axios from 'axios';
+import {traktApiKey} from './constants';
 
 const traktBaseUrl = 'https://api.trakt.tv/';
-const traktApiKey =
-  '869dd92637f9ee25d80033167a68d28f33ce045ee716d9094ba3d148826b3561';
 
 const farnatbaseUrl = 'http://webservice.fanart.tv/v3/movies/';
 export const farnatKey = 'b7e747b87e70624c052f265374f115eb';
