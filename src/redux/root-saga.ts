54c4f6da4326a1a11250b34652ebc3e1d15f487f
David Ferreira <davidferreira.m1@outlook.com>
2021-05-29 12:31:45 -0300

    chamada de authtoken saga

--- src/redux/root-saga.ts
+++ src/redux/root-saga.ts
@@ -1,15 +1,22 @@
 import {all, takeLatest} from 'redux-saga/effects';
+// movies
 import {PopularMovieTypes} from './movies/popular/types';
 import {pupularMovieRequestSaga} from './movies/popular/sagas';
 import {RecommendedMovieTypes} from './movies/recommended/types';
 import {recommendedMovieRequestSaga} from './movies/recommended/sagas';
 import {TrendingMovieTypes} from './movies/trending/types';
 import {trendingMovieRequestSaga} from './movies/trending/sagas';
+// auth
+import {AuthTokenypes} from './auth/types';
+import {getTokenRequestSaga} from './auth/sagas';
 
 export default function* rootSagas(): any {
   return yield all([
+    // MOVIES LIST
     takeLatest(PopularMovieTypes.LOAD_REQUEST, pupularMovieRequestSaga),
     takeLatest(RecommendedMovieTypes.LOAD_REQUEST, recommendedMovieRequestSaga),
     takeLatest(TrendingMovieTypes.LOAD_REQUEST, trendingMovieRequestSaga),
+    // AUTH TOKEN
+    takeLatest(AuthTokenypes.LOAD_REQUEST, getTokenRequestSaga),
   ]);
 }
