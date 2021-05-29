c7b13019e5e1f45235daa001c9f5ab9c62cdd72a
David Ferreira <davidferreira.m1@outlook.com>
2021-05-29 12:27:14 -0300

    vari[aveis de acesso

---
+++ src/http-client/constants.ts
@@ -0,0 +1,10 @@
+export const traktApiKey =
+  '869dd92637f9ee25d80033167a68d28f33ce045ee716d9094ba3d148826b3561';
+
+export const clientSecret =
+  '2c7b9fd77188f0b8681fc30a906d810c84739fc07d3ff830c3f00b190a3ef15e';
+
+export const codeApplication = '0EAB0D43';
+export const redirectUri = 'urn:ietf:wg:oauth:2.0:oob';
+
+export const farnatKey = 'b7e747b87e70624c052f265374f115eb';
