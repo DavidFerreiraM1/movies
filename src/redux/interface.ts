30207b5bc31aed06270b94a1181d1e035efcdd27
David Ferreira <davidferreira.m1@outlook.com>
2021-05-29 12:30:54 -0300

    reducer de token adicionado

--- src/redux/interface.ts
+++ src/redux/interface.ts
@@ -1,9 +1,11 @@
 import {PopularMovieState} from './movies/popular/types';
 import {RecommendedMovieState} from './movies/recommended/types';
 import {TrendingMovieState} from './movies/trending/types';
+import {AuthTokenState} from './auth/types';
 
 export interface AppStore {
   popularMovieReducer: PopularMovieState;
   recommendedMovieReducer: RecommendedMovieState;
   trendingMovieReducer: TrendingMovieState;
+  authTokenReducer: AuthTokenState;
 }
--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,10 +1,14 @@
 import {combineReducers} from 'redux';
+
 import {popularMovieReducer} from './movies/popular';
 import {recommendedMovieReducer} from './movies/recommended';
 import {trendingMovieReducer} from './movies/trending';
+
+import {authTokenReducer} from './auth';
 
 export default combineReducers({
   popularMovieReducer,
   recommendedMovieReducer,
   trendingMovieReducer,
+  authTokenReducer,
 });
