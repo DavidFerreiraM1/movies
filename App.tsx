45eda316ad0b27744263f7695bd81e7c8ad8af9f
David Ferreira <davidferreira.m1@outlook.com>
2021-05-30 11:42:18 -0300

    rotedor adicionado

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {PrincialScreen} from './src/features';
+import {RootRouter} from './src/features/routes';
 import store from './src/redux';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PrincialScreen />
+      <RootRouter />
     </Provider>
   );
 }
